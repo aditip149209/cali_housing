57707634050ad4fdd8f9f318740bab92a0599858
Aditi Pandey <aditip149209@gmail.com>
2024-07-14 13:39:40 +0530

    committed changes in app.py

--- app.py
+++ app.py
@@ -13,13 +13,19 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api',methods = ['POST'])
-
 def predict_api():
     data = request.json['data']
     #stores input in a json object
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
     new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    #(1,-1) indicates that this is a single data point that is being posted i.e a row
+    output = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
 
- 
+if __name__=="__main__":
+    app.run(debug=True)
+
+_
 
